c14de797169e11c3111d7e52e0cfaba7294d74b7
ckline2 <ckline2@mail.csuchico.edu>
2018-10-10 16:42:44 -0700

    Removable list

--- main.cpp
+++ main.cpp
@@ -6,15 +6,20 @@ using namespace std;
 
 int main()
 {
-        vector <string> list; //array of 5 strings
+        vector <string> list; //vector 
         int numItems = 0;
         char input;
         string num1;
+        string item;
 
         do {
                 cout<<"\n==GROCERY LIST MANAGER==";
                 cout<<"\nEnter your choice: ";
                 cout<<"\n (A)dd an item";
+                if (list.size()>=1)
+                {
+                cout<<"\n (D)elete last item";
+                }
                 cout<<"\n (Q)uit";
                 cout<<"\nYour choice (A/Q): ";
                 cin>>input;
@@ -23,6 +28,12 @@ int main()
                         cout<<"What is the item?\n";
                         cin>>num1;
                         list.push_back(num1);
+                }        
+                else if ((input == 'D' || input == 'd') && list.size() > 0)
+                {
+                        item = list.back();
+                        list.pop_back();
+                        cout<<item<<" was deleted from the list.\n";
                 }
         } while (input != 'Q' && input != 'q');
         if(list.size()==0)
